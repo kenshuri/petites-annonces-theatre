e0257fc8ce5c65ce5f06632de637525012b99094
alexis <mailalesci@gmail.com>
2024-06-16 12:53:14 +0200

    BUILD: all alowedd host

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%&qzlpn#0n8bmy+8-m5!e6vnp!k_q#i)!_1c-csr*vr_lcy&xh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
